88d909a3a8265c4df1652ca56fd323beebfe78b9
Arthur Zachow <arthur.zachow@gmail.com>
2016-06-23 06:43:56 -0300

    Makefile ready

--- Snake/src/snake1.c
+++ Snake/src/snake1.c
@@ -10,39 +10,6 @@ int nivelId = 0;
 char nivel[500];
 int colunas;
 
-/*int main2()
-{
-	int isGameOver = 0, quit = 0;
-	Cobra *head, *cobra1, *cobra2, *cobra3;
-	
-	head = malloc(sizeof(Cobra));
-	cobra1 = malloc(sizeof(Cobra));
-	cobra2 = malloc(sizeof(Cobra));
-	cobra3 = malloc(sizeof(Cobra));
-	
-	head->pos = 55;
-	head->next = cobra1;
-	cobra1->pos = 0;
-	cobra1->next = cobra2;
-	cobra2->pos = 0;
-	cobra2->next = cobra3;
-	cobra3->pos = 0;
-	cobra3->next = NULL;
-	
-	CarregaNivel();
-	ImprimeMapa(head);
-	
-	do
-	{
-		quit = Movimentacao(head);
-		system("cls");
-		isGameOver = ImprimeMapa(head);
-	}while((isGameOver != 1) && (quit != 1));
-	puts("Game over");
-	getch();
-	return 0;
-}*/
-
 int main()
 {
 		int op;
