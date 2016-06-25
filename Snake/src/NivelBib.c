076a82efd1da7348be1d0c4187c70ee9ed9095fe
HenriqueKRosa <henriquekrosa@hotmail.com>
2016-06-25 16:08:37 -0300

    Update NivelBib.c

--- Snake/src/NivelBib.c
+++ Snake/src/NivelBib.c
@@ -195,3 +195,35 @@ void Print_Map(char Map_Name[20])
 	}
 	fclose(Map_File);
 }
+void PosicoesLivres(Cobra* condutor, int* PosLivres, int* PosOcupadas, int NPosOcupadas)                 //Preenche array com as posições livres no mapa.
+{
+    int PosDaCobra[500], cont = 0, k = 0, j = 0, TamanhoCobra;
+    while(condutor != NULL)
+    {
+        PosDaCobra[cont] = condutor->pos;
+        condutor = condutor->next; //Vai pro próximo elemento da lista
+        ++;
+}
+    TamanhoCobra = cont;
+    while(nivel[k] != '\0')
+    {
+        if(nivel[k] == ' ' && !(IsKPosDoArray(k, PosDaCobra, TamanhoCobra)) && !(IsKPosDoArray(k, PosOcupadas, NPosOcupadas)))
+        {
+            PosLivres[j] = k;
+            j++;
+        }
+        k++;
+    }
+}
+int IsKPosDoArray(int k, int* ArrayDePosições, int TamanhoArray)         //Verifica se um número K é alguma posição da cobra.
+{
+    int i, boolean = 0;
+    for(i = 0; i < TamanhoArray; i++)
+    {
+        if(k == ArrayDePosições[i])
+        {
+            boolean = 1;
+        }
+    }
+    return boolean;
+}
